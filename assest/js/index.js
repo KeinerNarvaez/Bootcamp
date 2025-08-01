246e42a1714ea4354385ce8af07c4690d75c1ae0
keinernarvaez <ckeinercano@gmail.com>
2025-08-01 16:20:33 -0500

    fin

--- assest/js/index.js
+++ assest/js/index.js
@@ -1,26 +1,5 @@
-
 const ApiUrl = "http://127.0.0.1:8080/Player";
-
-const crearJugadores = async () => {
-    
-    let name = []
-    
-
-    try {
-        const respuesta = await fetch(ApiUrl + "/create", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                name: name
-            })
-        });
-        data = await respuesta.json();
-    } catch (Error) {
-        console.error("hubo un error al registrar los jugadores")
-    }
-};
+let array=[]
 document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("number").addEventListener("click", function () {
             let numero = this.value;
@@ -31,11 +10,34 @@ document.addEventListener("DOMContentLoaded", function () {
                 let etiquetaJugador = document.createElement('h5');
                 etiquetaJugador.innerText = "jugador " + index;
                 let etiquetaEntrada = document.createElement("input");
+                etiquetaEntrada.setAttribute("id","inputName"+index)
+                array.push("inputName"+index)
+                console.log(array);
                 padre.appendChild(etiquetaJugador);
                 padre.appendChild(etiquetaEntrada);
             }
         })
     });
+
+const crearJugadores = async () => {
+    let array = document.getElementById("inputName1")    
+
+    try {
+        const respuesta = await fetch(ApiUrl + "/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                name: array
+            })
+        });
+        data = await respuesta.json();
+    } catch (Error) {
+        console.error("hubo un error al registrar los jugadores")
+    }
+};
+
 const obtenerJugadores = async () => {
     try {
         const respuesta = await fetch(ApiUrl + "/", {
@@ -52,5 +54,5 @@ const obtenerJugadores = async () => {
 };
         document.getElementById("jugar").addEventListener("click", function () {
             crearJugadores()
-            // location.href = "../juego.html"
+            location.href = "../juego.html"
         })
